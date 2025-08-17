374a98e4d175f240bdf367b53d5d42c54eb9aefd
rehmandev268 <rehmandev268@gmail.com>
2025-08-17 12:28:55 +0500

    Minor fixes

--- lib/features/auth/presentation/screens/ui_login.dart
+++ lib/features/auth/presentation/screens/ui_login.dart
@@ -9,151 +9,176 @@ class LoginScreen extends ConsumerWidget {
     final currentIndex = ref.watch(bottomNavIndexProvider);
     final notifier = ref.read(bottomNavIndexProvider.notifier);
     return Scaffold(
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(
-          horizontal: getProportionateScreenWidth(34),
-          vertical: getProportionateScreenHeight(30),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.asset(AppAssets.onboardingImage, fit: BoxFit.fill),
-            SizedBox(height: getProportionateScreenHeight(45)),
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'Any topic. Anytime.',
-                    style: AppTextStyles.headingTextStyle.copyWith(
-                      color: AppColors.brownColor,
-                      height: 40 / 32,
-                      fontSize: 32,
+      body: LayoutBuilder(
+        builder:
+            (context, constraints) => SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                child: IntrinsicHeight(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: getProportionateScreenWidth(34),
+                      vertical: getProportionateScreenHeight(20),
                     ),
-                  ),
-                  TextSpan(
-                    text: '\nYour way.',
-                    style: AppTextStyles.headingTextStyle.copyWith(
-                      color: AppColors.blueColor,
-                      fontSize: 32,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Image.asset(
+                          AppAssets.onboardingImage,
+                          fit: BoxFit.fill,
+                        ),
+                        SizedBox(height: getProportionateScreenHeight(45)),
+                        RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: 'Any topic. Anytime.',
+                                style: AppTextStyles.headingTextStyle.copyWith(
+                                  color: AppColors.brownColor,
+                                  height: 40 / 32,
+                                  fontSize: 32,
+                                ),
+                              ),
+                              TextSpan(
+                                text: '\nYour way.',
+                                style: AppTextStyles.headingTextStyle.copyWith(
+                                  color: AppColors.blueColor,
+                                  fontSize: 32,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
 
-            SizedBox(height: getProportionateScreenHeight(65)),
-            Text(
-              textAlign: TextAlign.center,
-              'Sign in to continue with',
-              style: AppTextStyles.bodyTextStyle.copyWith(
-                color: AppColors.brownColor,
-                fontSize: getProportionateScreenHeight(16),
-                fontWeight: FontWeight.w500,
-                wordSpacing: 1.4,
-              ),
-            ),
-            SizedBox(height: getProportionateScreenHeight(24)),
-            if (Platform.isIOS) ...[
-              PrimaryButton(
-                text: 'Apple',
-                icon: SvgPicture.asset(
-                  AppAssets.appleIcon,
-                  fit: BoxFit.scaleDown,
-                ),
-                height: getProportionateScreenHeight(56),
-                onPressed: () async {
-                  if (currentIndex != 0) {
-                    notifier.state = 0;
-                  }
-                  await locator.get<AuthServices>().handleAppleAuth(context);
-                },
-                textColor: AppColors.blackColor,
-                backgroundColor: AppColors.whiteColor,
-              ),
-              SizedBox(height: getProportionateScreenHeight(12)),
-            ],
-            PrimaryButton(
-              text: 'Google',
-              height: getProportionateScreenHeight(56),
-              icon: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                child: SvgPicture.asset(
-                  AppAssets.googleIcon,
-                  fit: BoxFit.scaleDown,
-                ),
-              ),
-
-              onPressed: () {
-                if (currentIndex != 0) {
-                  notifier.state = 0;
-                }
-                locator.get<AuthServices>().handleGoogleAuth(context);
-              },
-              textColor: AppColors.blackColor,
-              backgroundColor: AppColors.whiteColor,
-            ),
-
-            SizedBox(height: getProportionateScreenHeight(30)),
-
-            RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                style: AppTextStyles.bodyTextStyle.copyWith(
-                  color: const Color(0xff8E8E93),
-                  fontSize: 12,
-                  fontWeight: FontWeight.w400,
-                ),
-                children: [
-                  const TextSpan(text: 'By continuing, you agree to our'),
-                  TextSpan(
-                    text: ' Terms & Conditions',
-                    style: AppTextStyles.bodyTextStyle.copyWith(
-                      color: AppColors.blackColor,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      decoration: TextDecoration.underline,
-                    ),
-                    recognizer:
-                        TapGestureRecognizer()
-                          ..onTap = () async {
-                            final url = Uri.parse('https://yourapp.com/terms');
-                            if (await canLaunchUrl(url)) {
-                              await launchUrl(
-                                url,
-                                mode: LaunchMode.externalApplication,
+                        SizedBox(height: getProportionateScreenHeight(65)),
+                        Text(
+                          textAlign: TextAlign.center,
+                          'Sign in to continue with',
+                          style: AppTextStyles.bodyTextStyle.copyWith(
+                            color: AppColors.brownColor,
+                            fontSize: getProportionateScreenHeight(16),
+                            fontWeight: FontWeight.w500,
+                            wordSpacing: 1.4,
+                          ),
+                        ),
+                        SizedBox(height: getProportionateScreenHeight(24)),
+                        if (Platform.isAndroid) ...[
+                          PrimaryButton(
+                            text: 'Apple',
+                            icon: SvgPicture.asset(
+                              AppAssets.appleIcon,
+                              fit: BoxFit.scaleDown,
+                            ),
+                            height: getProportionateScreenHeight(56),
+                            onPressed: () async {
+                              if (currentIndex != 0) {
+                                notifier.state = 0;
+                              }
+                              await locator.get<AuthServices>().handleAppleAuth(
+                                context,
                               );
+                            },
+                            textColor: AppColors.blackColor,
+                            backgroundColor: AppColors.whiteColor,
+                          ),
+                          SizedBox(height: getProportionateScreenHeight(12)),
+                        ],
+                        PrimaryButton(
+                          text: 'Google',
+                          height: getProportionateScreenHeight(56),
+                          icon: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 4,
+                              horizontal: 8,
+                            ),
+                            child: SvgPicture.asset(
+                              AppAssets.googleIcon,
+                              fit: BoxFit.scaleDown,
+                            ),
+                          ),
+
+                          onPressed: () {
+                            if (currentIndex != 0) {
+                              notifier.state = 0;
                             }
+                            locator.get<AuthServices>().handleGoogleAuth(
+                              context,
+                            );
                           },
-                  ),
-                  const TextSpan(text: ' and\n'),
-                  TextSpan(
-                    text: ' Privacy Policy.',
-                    style: AppTextStyles.bodyTextStyle.copyWith(
-                      color: AppColors.blackColor,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w400,
-                      decoration: TextDecoration.underline,
+                          textColor: AppColors.blackColor,
+                          backgroundColor: AppColors.whiteColor,
+                        ),
+                        SizedBox(height: getProportionateScreenHeight(12)),
+                        Spacer(),
+                        RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            style: AppTextStyles.bodyTextStyle.copyWith(
+                              color: const Color(0xff8E8E93),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            children: [
+                              const TextSpan(
+                                text: 'By continuing, you agree to our',
+                              ),
+                              TextSpan(
+                                text: ' Terms & Conditions',
+                                style: AppTextStyles.bodyTextStyle.copyWith(
+                                  color: AppColors.blackColor,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  decoration: TextDecoration.underline,
+                                ),
+                                recognizer:
+                                    TapGestureRecognizer()
+                                      ..onTap = () async {
+                                        final url = Uri.parse(
+                                          'https://yourapp.com/terms',
+                                        );
+                                        if (await canLaunchUrl(url)) {
+                                          await launchUrl(
+                                            url,
+                                            mode:
+                                                LaunchMode.externalApplication,
+                                          );
+                                        }
+                                      },
+                              ),
+                              const TextSpan(text: ' and\n'),
+                              TextSpan(
+                                text: ' Privacy Policy.',
+                                style: AppTextStyles.bodyTextStyle.copyWith(
+                                  color: AppColors.blackColor,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                  decoration: TextDecoration.underline,
+                                ),
+                                recognizer:
+                                    TapGestureRecognizer()
+                                      ..onTap = () async {
+                                        final url = Uri.parse(
+                                          'https://yourapp.com/privacy',
+                                        ); // replace with real URL
+                                        if (await canLaunchUrl(url)) {
+                                          await launchUrl(
+                                            url,
+                                            mode:
+                                                LaunchMode.externalApplication,
+                                          );
+                                        }
+                                      },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    recognizer:
-                        TapGestureRecognizer()
-                          ..onTap = () async {
-                            final url = Uri.parse(
-                              'https://yourapp.com/privacy',
-                            ); // replace with real URL
-                            if (await canLaunchUrl(url)) {
-                              await launchUrl(
-                                url,
-                                mode: LaunchMode.externalApplication,
-                              );
-                            }
-                          },
                   ),
-                ],
+                ),
               ),
             ),
-          ],
-        ),
       ),
     );
   }
